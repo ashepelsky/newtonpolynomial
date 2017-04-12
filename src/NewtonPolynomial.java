a25c98331a64c81edc9cde521ce880e2c016694d
Alexandr Shepelsky <ashepelsky@ancestry.com>
2017-04-12 19:06:32 +0300

    Refactored Interpolation calculation method

--- src/NewtonPolynomial.java
+++ src/NewtonPolynomial.java
@@ -6,46 +6,40 @@ public class NewtonPolynomial {
     private double y[] = PropertyLoader.loadInitialValues('y');
     private int degree = x.length;
 
-    //TODO: Refactor method
     private void calculateInterpolation() {
         double temp1 = y[0];
-        double temp2 = y[1];
+        double temp2;
 
-        int k = 2;
+        int k = 1;
 
         for (int i = 1; i < degree; i++) {
             for (int j = 0; j < degree - i; j++) {
-
+                temp2 = y[j + k];
                 y[j + i] = (temp2 - temp1) / (x[j + i] - x[j]);
-                //System.out.println((x[j + i] - x[j]));
+                System.out.print(y[j + i] + " \t");
+
                 temp1 = temp2;
-                try {
-                    temp2 = y[j + k];
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    break;
-                }
             }
+            System.out.println(" ");
             k++;
             temp1 = y[i];
-            try {
-                temp2 = y[i + 1];
-            } catch (ArrayIndexOutOfBoundsException e) {
-            }
         }
-        //System.out.println("test");
-
     }
 
     private double calculatePolynomial(double x) {
         double result = 1;
         for (int i = degree - 1; i > -1; i--) {
-            result*= y[i] + (x - this.x[i]);
+            result *= y[i] + (x - this.x[i]);
         }
         return result;
     }
 
+    private void printFormula() {
+    }
+
     public double calculate(double x) {
         calculateInterpolation();
+        printFormula();
         return calculatePolynomial(x);
     }
 
