a99fc49fe5b0ef9dd549087b631bd6c81da570da
ashepelsky <alex.shepelskyy@gmail.com>
2017-04-26 21:50:04 +0300

    Created controller for calculating X values

--- src/main/java/Webapp/NewtonPolynomialController.java
+++ src/main/java/Webapp/NewtonPolynomialController.java
@@ -1,6 +1,8 @@
 package Webapp;
 
 import Core.NewtonPolynomial;
+import Core.TheoreticalError;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +15,30 @@ import java.util.Map;
 @RestController
 public class NewtonPolynomialController {
 
+
     @RequestMapping("/")
     public Map<String, Object> home() {
         Map<String, Object> model = new HashMap<String, Object>();
+
         NewtonPolynomial service = new NewtonPolynomial();
 
-        model.put("interpolation", service.getInterpolationValues().toString());
+        model.put("x",service.getX());
+        model.put("y",service.getY());
+        model.put("interpolation", service.getInterpolationValues());
         model.put("polynomialFormula", service.getPolynomialFormula());
+
+        return model;
+    }
+
+    @RequestMapping("/api/{x}")
+    public Map<String, Object> xValues(@PathVariable double x) {
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        NewtonPolynomial service = new NewtonPolynomial();
+        service.getInterpolationValues();
+
+        model.put("polynomialValue",service.calculatePolynomial(x));
+        model.put("theoreticalError", new TheoreticalError().getTheoreticalError(x));
 
         return model;
     }
